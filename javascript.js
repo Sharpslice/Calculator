af580970ab2addc3e20b9fa9fcdc95d870bd12d2
sharpslice <davidlein1440p@gmail.com>
2024-06-20 13:38:48 -0700

    added basic operatation functions

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+let num1;
+let num2;
+let operator;
+
+function add (x,y){
+    return x + y;
+}
+function subtract(x,y){
+    return x-y;
+}
+function multiply(x,y){
+    return x*y;
+}
+function divide(x,y){
+    return x/y;
+}
+
+function operate(num1, num2,operator){
+    switch (operator){
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+            
+    }
+}
